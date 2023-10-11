e0ce3817bd7ad6f6aa9355c586e318e0e362a4bc
Dario Sandoval Evert <djsandoval@falabella.cl>
2023-10-11 13:07:34 -0300

    feat: package logging

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/dsandovale/refactored-journey/random"
+import (
+	"time"
+
+	"github.com/dsandovale/refactored-journey/logging"
+	"github.com/dsandovale/refactored-journey/random"
+)
 
 func main() {
+	logger := logging.New(time.RFC3339, true)
+	logger.Log("info", "starting up service")
 	random.Calcule()
+	logger.Log("warning", "no tasks found")
+	logger.Log("error", "exiting: no work performed")
 }
